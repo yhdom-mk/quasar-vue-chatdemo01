35f767537ed2a1968d45b942e18155783409fd61
Mski Ydom <lcydome.msk12@gmail.com>
2022-05-31 03:14:52 +0900

    fireconfig 追加

---
+++ fireconifg.js
@@ -0,0 +1,9 @@
+const fireConfig = {
+  apiKey: "AIzaSyDFMMLCjVvA4GQTMjRlEkxxiQNBwcc6IAE",
+  authDomain: "quasarchatapp-demo01.firebaseapp.com",
+  databaseURL: "https://quasarchatapp-demo01-default-rtdb.firebaseio.com",
+  projectId: "quasarchatapp-demo01",
+  storageBucket: "quasarchatapp-demo01.appspot.com",
+  messagingSenderId: "1014884811982",
+  appId: "1:1014884811982:web:e58e3cba4c4fe443f7fc9f"
+}
