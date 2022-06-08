ce0bfb81bd1e4bff92935d8fab4a2fdca2964b75
Mski Ydom <lcydome.msk12@gmail.com>
2022-06-08 19:53:10 +0900

    module 修正

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,7 +3,7 @@ import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import fireConfig from '../../firebaseconfig';
+import { fireConfig } from '../../firebaseconfig';
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 // export default boot(async (/* { app, router, ... } */) => {
@@ -22,4 +22,3 @@ let firebaseAuth = firebase.auth();
 let firebaseDb = firebase.firestore();
 
 export { firebaseAuth, firebaseDb }
-
